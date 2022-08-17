f320895d3e44036bafd9827eccbacdf7368108d2
Kolte <2107262@cognizant.com>
2022-08-17 23:35:32 +0530

    ready for deployment!!

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -8,7 +8,7 @@ async function handler(req, res) {
     const data = req.body;
 
     const client = await MongoClient.connect(
-      'mongodb+srv://taskApp:taskApp123@atlascluster.6wuy8xk.mongodb.net/meetups'
+      'mongodb+srv://taskApp:taskApp123@atlascluster.6wuy8xk.mongodb.net/meetups?retryWrites=true&w=majority'
     );
     const db = client.db();
 
